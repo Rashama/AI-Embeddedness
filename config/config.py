36a5ec7f5abd22ff2d41057cfd8f3a18e73e8445
Rasha Alahmad <47429639+Rashama@users.noreply.github.com>
2025-05-10 19:25:10 +0300

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+# Model paths
+sentiment_model_path = "/Users/rasha/..."
+translation_model_path = "/Users/rasha/A.."
+image_model_path = "/Users/rasha/A.."
+
+# API Keys
+OPENAI_API_KEY = ""
+GEMINI_API_KEY = ""
